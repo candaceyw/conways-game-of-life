c39369fa898db7bbca9b4fe2be23f8caee183184
candacewilson <candaceyw@gmail.com>
2020-07-29 19:49:14 -0400

    Updates

---
+++ precache-manifest.6f42aed36cd29706d1181efd22b09785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e656713e2ad15589b92f471417e00e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654156ecbc244c4265cb",
+    "url": "/static/css/main.e2571859.chunk.css"
+  },
+  {
+    "revision": "234d18a39b70e558bd5b",
+    "url": "/static/js/2.6599028e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6599028e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654156ecbc244c4265cb",
+    "url": "/static/js/main.63896561.chunk.js"
+  },
+  {
+    "revision": "9d701863dc2852aa5de4",
+    "url": "/static/js/runtime-main.3c313d4f.js"
+  },
+  {
+    "revision": "bbf81b7f48064d1558e8e27002bef451",
+    "url": "/static/media/pixelbg.bbf81b7f.jpg"
+  }
+]);
